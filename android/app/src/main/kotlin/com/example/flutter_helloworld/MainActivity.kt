00445bbcb43e51ab23607aaf0c60c8839e434e0a
Dat, Vu Ba Tat <dat.vbt@homecredit.vn>
2020-07-31 09:57:43 +0700

    Update splash screen

--- android/app/src/main/kotlin/com/example/flutter_helloworld/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_helloworld/MainActivity.kt
@@ -1,6 +1,21 @@
 package com.example.flutter_helloworld
 
+import android.os.Build
+import android.os.Bundle
+import android.view.WindowManager
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //make transparent status bar
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            window.statusBarColor = 0x00000000
+        }
+    }
+
+    public override fun onResume() {
+        super.onResume()
+        window.clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
+    }
 }
